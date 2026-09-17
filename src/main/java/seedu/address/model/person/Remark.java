1a9b68fdd2c54733dbe49103531b01aa5243b9f5
Tristan914684 <tristanlyn44@gmail.com>
2026-09-17 23:20:15 +0800

    Add javadoc comment for Remark constructor

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,11 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Constructs a {@code Remark} with the given remark.
+     *
+     * @param remark the remark text
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
